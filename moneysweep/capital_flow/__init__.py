310c03203fd19ff8042fd7314f318a483db4bd39
jotaele44 <jorge.luis1352@gmail.com>
2026-09-16 00:36:38 -0400

    feat: expose macro reconciliation API

--- moneysweep/capital_flow/__init__.py
+++ moneysweep/capital_flow/__init__.py
@@ -1,5 +1,13 @@
 """MoneySweep V2 capital-flow reconciliation core."""
 
+from .macro import (
+    MacroCandidateState,
+    MacroClosureState,
+    MacroIdentityClosure,
+    MacroObservation,
+    adjudicate_latest_manifestation,
+    published_identity_closure,
+)
 from .model import (
     CertificationState,
     FlowDirection,
@@ -13,9 +21,15 @@ from .model import (
 __all__ = [
     "CertificationState",
     "FlowDirection",
+    "MacroCandidateState",
+    "MacroClosureState",
+    "MacroIdentityClosure",
+    "MacroObservation",
     "MeasurementType",
     "RetentionState",
+    "adjudicate_latest_manifestation",
     "arithmetic_closure",
     "derive_gdp_gnp_gap",
+    "published_identity_closure",
     "retention_rates",
 ]
